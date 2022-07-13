8e4d383b7683033f8d1a784514bc0ae3c9a4a80e
SunGody <7455488@qq.com>
2022-07-13 16:02:34 +0800

    update main

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+//引入字体文件及初始化项目配置
+import "@/assets/font/iconfont.css";
+import "@/assets/css/reset.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
